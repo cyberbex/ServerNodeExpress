cfb11dff365e854f9018a6b441c335a6d853e64f
Bruno Migliorini <cyberbex@gmail.com>
2021-07-17 00:41:38 -0300

    llkjlkj

--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const path = require('path');
 
 //----declaração do recibimento de um objeto pelo corpo da requisição---
 app.use( express.urlencoded({extended:true}));
+
+//--conteudo statico
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+//caminho para views usando o ejs(tipo html com super poderes)
+app.set('views',path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
 app.use(routes);
+
 
 app.listen(3000,()=>{
     console.log('Acessar http://localHost:3000');
