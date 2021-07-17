da96828f2f44161c3ccdc3e5669cacddf60dffdc
Bruno Migliorini <cyberbex@gmail.com>
2021-07-17 15:56:59 -0300

    mongodb

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 //Sistema full MVC(Backend e frontend tudo junto)
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=>{
+        console.log('Conectei a base de dados');
+        app.emit('pronto');
+    })
+    .catch(e=>console.log(e));
+
+
+
+
 const routes = require('./routes');
 const path = require('path');
+const meuMiddleware = require('./src/middlewares/middleware');
 
 //----declaração do recibimento de um objeto pelo corpo da requisição---
 app.use( express.urlencoded({extended:true}));
@@ -14,10 +29,16 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views',path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+//todas as requisições, em todas as rotas, passam pelo middleware(global)
+app.use(meuMiddleware);
+
 app.use(routes);
 
-
-app.listen(3000,()=>{
-    console.log('Acessar http://localHost:3000');
-    console.log('servidor rodando na porta 3000')
+app.on('pronto', ()=>{
+    app.listen(3000,()=>{
+        console.log('Acessar http://localHost:3000');
+        console.log('servidor rodando na porta 3000')
+    });
+    
 });
+
